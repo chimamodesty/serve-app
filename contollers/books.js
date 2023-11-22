6e6c2a41220cb75ab8f9cc744ae1c6a6833c1fc0
chimamodesty <chimavic2000@gmail.com>
2023-11-22 23:53:31 +0100

    created PUT and DELETE endpoints

--- contollers/books.js
+++ contollers/books.js
@@ -1,4 +1,6 @@
 const mongodb = require('../db/connect')
+var ObjectId = require('mongodb').ObjectId
+
 
 const getAll = async( req, res, next) => {
     const result = await mongodb
@@ -34,4 +36,29 @@ const createbooks = async(req, res) => {
 
 }
 
-module.exports = {getAll, createbooks}
+const updatebooks = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id)
+    await mongodb.getDb().db().collection('books').updateOne(
+        {'_id': userId},
+        {$set: {...req.body}} 
+        )
+        .then(result => 
+            res.status(204).json(result))
+        .catch(err => res.status(400).json({'error': err.message}))
+}
+
+
+const deletebooks = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id)
+    
+    await mongodb.getDb().db().collection('books').deleteOne({'_id': userId})
+        .then(result => res.status(200).json({"deletedId": req.params.id, ...result}))
+}
+
+module.exports = {
+    getAll, 
+    createbooks,
+    updatebooks,
+    deletebooks
+
+ }
--- routes/books.js
+++ routes/books.js
@@ -3,5 +3,7 @@ const bookControllers = require("../contollers/books")
 
 routes.post("/", bookControllers.createbooks)
 routes.get("/", bookControllers.getAll)
+routes.put("/:id", bookControllers.updatebooks)
+routes.delete("/:id", bookControllers.deletebooks)
 
 module.exports = routes
