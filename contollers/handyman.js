3f9b395c20659825771425d3eb10efa59515d73e
davidogbaudu <ogbaudu.dc@gmail.com>
2023-11-23 10:02:45 +0100

    created put and update routes, seperatd rest files

--- contollers/handyman.js
+++ contollers/handyman.js
@@ -1,4 +1,6 @@
 const mongodb = require('../db/connect')
+const  ObjectId = require("mongodb").ObjectId
+
 
 const getAll = async( req, res, next) => {
     const result = await mongodb
@@ -33,5 +35,36 @@ const createHandyman = async(req, res) => {
         }
 
 }
+const getProfession = async (req, res, next) => {
+    const handymanProfession = req.params.profession;
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("handyman")
+      .find({ profession: handymanProfession });
+    result.toArray().then((lists) => {
+      res.status(200).json(lists);
+    });
+  };
 
-module.exports = {getAll, createHandyman}
+  const updateHandyman = async (req, res) => {
+    const handymanId = new ObjectId(req.params.id);
+
+    const handyman = {
+        name: req.body.name,
+        profession: req.body.profession,
+        city: req.body.city,
+        tel: req.body.tel
+    };
+    const response = await mongodb
+    .getDb()
+    .db()
+    .collection('handyman')
+    .replaceOne({_id: handymanId}, handyman);
+    if(response.modifiedCount > 0){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while updating the ward.')
+    }
+  }
+module.exports = {getAll, createHandyman, getProfession, updateHandyman}
--- routes/handyman.js
+++ routes/handyman.js
@@ -3,5 +3,8 @@ const handymanControllers = require("../contollers/handyman")
 
 routes.post("/", handymanControllers.createHandyman)
 routes.get("/", handymanControllers.getAll)
+routes.get("/:profession", handymanControllers.getProfession)
+routes.put("/:id", handymanControllers.updateHandyman)
+routes.delete("/:id", handymanControllers.getProfession)
 
 module.exports = routes
